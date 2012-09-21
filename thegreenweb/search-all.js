1544b7216ec9df4451dca4fe87a6e861d42f1cd1
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2012-09-21 17:51:50 +0200

    Show a green link for all external links on page Version 1.2.5

--- thegreenweb/search-all.js
+++ thegreenweb/search-all.js
@@ -11,20 +11,21 @@
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
         if (request.data){
-          console.log(request.data);
             data = request.data;
-            console.log($("a :not(.Cleanbits > a)").length);
+           
             $("a").each(function (i) {
-                if(data[i]){
-                  if(data[i].green){
+              var loc = $(this).attr('href');
+              var strippedurl = getUrl(loc);
+              if (loc && strippedurl) {
+                if (data[strippedurl]) {
+                  
+                  if(data[strippedurl].green){
                     $(this).addClass('tgwf_green');
+                  } else {
+                    $(this).addClass('tgwf_grey');
                   }
-                  console.log(data[i]);
-                    $(this).find('.Cleanbits').first().html(getResult(data[i]));
-                    if(data[i].poweredby) {
-                       $(this).find('.Cleanbits').parent().css('background', '#DBFA7F');
-                    }
                 }
+              }
             });
             sendResponse({});
         }else{
@@ -41,27 +42,26 @@ $(document).ready(function() {
           // Green web search is disabled, return
           return;
         }
-        currenturl = getUrl(document.URL);
-        var locs = new Array();
-        console.log($("a").length);
-        $("a").each(function (i) {
-             var loc = $(this).attr('href');
-             var strippedurl = getUrl(loc);
-             //console.log(loc);
-             //console.log(getUrl(loc));
-             if(loc && strippedurl){
-              //console.log('valid url');
-               if(getUrl(loc) == currenturl){
-                 return true;
-               }
-               $(this).prepend('<span class="Cleanbits">' + getImage('greenquestion') + '&nbsp;</span>');
-               locs[i] = getUrl(loc); 
-             }             
-        });
-        if(locs.length > 0) {
-          console.log(locs);
-            chrome.extension.sendRequest({locs: locs}, function(response) {
-            });
-        }
+        getUrlsAndSendRequest();
     });
 });
+
+function getUrlsAndSendRequest()
+{
+  currenturl = getUrl(document.URL);
+  var locs = new Object();
+  $("a").each(function (i) {
+       var loc = $(this).attr('href');
+       var strippedurl = getUrl(loc);
+       if (loc && strippedurl) {
+         /*if (getUrl(loc) == currenturl) {
+           return true;
+         }*/
+         locs[strippedurl] = strippedurl 
+       }             
+  });
+  if(Object.keys(locs).length > 0) {
+      chrome.extension.sendRequest({locs: locs}, function(response) {
+      });
+  }
+}
