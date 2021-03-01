4a088675531eca0767365b9458fa57b8d06868a7
Chris Adams <chris@productscience.co.uk>
2021-03-01 18:15:40 +0100

    Switch off Google search result annotations

--- thegreenweb/search-google.js
+++ thegreenweb/search-google.js
@@ -1,5 +1,7 @@
 /*
  * Google search pagemod functions
+ * TODO - this needs to be updated to match the new async/await and fetch based
+ * approach
  *
  * @author Arend-Jan Tetteroo <aj@thegreenwebfoundation.org>
  * @copyright Cleanbits/The Green Web Foundation 2010-2020
@@ -8,16 +10,17 @@
 /**
  * On Request, find all hrefs and assign green or grey icon
  */
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.data){
+    function (request, sender, sendResponse) {
+        if (request.data) {
             var data = request.data;
             var links = $('.TGWF');
             $(links).each(function () {
                 var loc = getUrl($(this).parent().attr('href'));
                 if (data[loc]) {
                     $(this).html(getResultNode(data[loc], 'google').append('&nbsp;'));
-                    if(request.filter && data[loc].green === false) {
+                    if (request.filter && data[loc].green === false) {
                         // remove full result from the page
                         $(this).parents('.rc').hide();
                     }
@@ -30,11 +33,11 @@ chrome.runtime.onMessage.addListener(
 /**
  * If document is ready, find the urls to check
  */
-$(document).ready(function() {
-    chrome.storage.sync.get("tgwf_search_disabled", function(items) {
+$(document).ready(function () {
+    chrome.storage.sync.get("tgwf_search_disabled", function (items) {
         if (items && items.tgwf_search_disabled && items.tgwf_search_disabled === "1") {
-          // Green web search is disabled, return
-          return;
+            // Green web search is disabled, return
+            return;
         }
 
         var footer = document.getElementById("fbar");
@@ -43,7 +46,7 @@ $(document).ready(function() {
         (function checkLoop() {
             // Check if search results have 'greenweb' link
             var results = $('#res').find('.r > a');
-            if ( $('.TGWF').length !== results.length) {
+            if ($('.TGWF').length !== results.length) {
 
                 // Remove all tgwf links
                 $('.TGWF').remove();
@@ -52,12 +55,12 @@ $(document).ready(function() {
                 var locs = {};
                 $(results).each(function (i) {
                     // Add TGWF link to each google listing
-                    $(this).prepend($('<span>', { class: 'TGWF'}).append(getImageNode('greenquestion')).append('&nbsp;'));
+                    $(this).prepend($('<span>', { class: 'TGWF' }).append(getImageNode('greenquestion')).append('&nbsp;'));
                     var loc = getUrl($(this).attr('href'));
                     locs[loc] = loc;
                 });
                 if (Object.keys(locs).length > 0) {
-                    chrome.runtime.sendMessage({locs: locs}, function(response) {});
+                    chrome.runtime.sendMessage({ locs: locs }, function (response) { });
                 }
             }
             setTimeout(checkLoop, 100);
